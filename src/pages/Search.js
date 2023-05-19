d5b9d19bab704e37d13375edfd5d6367464c8666
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-05-19 11:34:59 -0500

    proj init

---
+++ src/pages/Search.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+function Search() {
+  const inputRef = useRef(null);
+  const [inputValue, setInputValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [userNameExists, setUserNameExists] = useState(true);
+  const navigate = useNavigate();
+
+  const token = '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';
+  const options = {headers: { Authorization: `Bearer ${token}`}};
+
+  let userEndpoint = `https://api.github.com/users/${inputValue}`;
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    inputRef.current.focus();
+    if (inputValue.length <= 0) {
+      setErrorMessage('Please write a username');
+      return;
+    }
+    if (!userNameExists) {
+      setErrorMessage('Invalid user name, try again');
+      return;
+    }
+    setErrorMessage('');
+    navigate(`user/${inputValue}`);
+    setInputValue('');
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const userResponse = await axios.get(userEndpoint, options);
+        setUserNameExists(true);
+        console.log(userResponse.data);
+      } catch (error) {
+        console.log(error);
+        setUserNameExists(false);
+      }
+    }
+
+    if (inputValue) getData();
+    // eslint-disable-next-line
+  }, [inputValue])
+
+  return (
+    <div className='search-form'>
+      <h1>GitHub Finder</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+            type='text'
+            className={ userNameExists ? 'user-name' : 'user-name invalid'} 
+            placeholder='User Name'
+            onChange={handleChange}
+            ref={inputRef}
+        />
+        <input
+            type='submit'
+            className='user-search'
+            value='SEARCH'
+        />
+      </form>
+      <p>{errorMessage}</p>
+    </div>
+  )
+}
+
+export default Search
---
+++ src/pages/User.js
@@ -0,0 +1,91 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+function User() {
+  const [userPFP, setUserPFP] = useState('');
+  const [userIRLName, setUserIRLName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userReposCount, setUserReposCount] = useState('');
+  const [userFollowersCount, setUserFollowersCount] = useState('');
+  const [userFollowingCount, setUserFollowingCount] = useState('');
+  const [userGitHubURL, setUserGitHubURL] = useState('');
+
+  const [userReposArray, setUserReposArray] = useState([]);
+
+  const {username} = useParams();
+
+  const token = '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';
+  const options = {headers: { Authorization: `Bearer ${token}`}};
+
+  let userEndpoint = `https://api.github.com/users/${username}`;
+  let userReposEndpoint = `https://api.github.com/users/${username}/repos`;
+
+  useEffect(() => {
+
+    const getData = async () => {
+      try {
+        const userResponse = await axios.get(userEndpoint, options);
+        const reposResponse = await axios.get(userReposEndpoint, options);
+        console.log(userResponse.data);
+        setUserPFP(userResponse.data.avatar_url);
+        setUserIRLName(userResponse.data.name);
+        setUserDescription(userResponse.data.bio);
+        setUserReposCount(userResponse.data.public_repos);
+        setUserFollowersCount(userResponse.data.followers);
+        setUserFollowingCount(userResponse.data.following);
+        setUserGitHubURL(userResponse.data.html_url);
+        console.log(reposResponse.data);
+        setUserReposArray(reposResponse.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (username) getData();
+    // eslint-disable-next-line
+  }, [username])
+
+  const dateOptions = { month: 'short', day: 'numeric', year: 'numeric' };
+
+  return (
+    <main>
+      <div className='container'>
+        <div className='user-info'>
+          <img src={userPFP} alt='the users pfp' />
+          <h2>{userIRLName}</h2>
+          <p>{userDescription}</p>
+          <div className='user-data'>
+            <div>
+              <span>{userReposCount}</span>
+              <p>Repositories</p>
+            </div>
+            <div>
+              <span>{userFollowersCount}</span>
+              <p>Followers</p>
+            </div>
+            <div>
+              <span>{userFollowingCount}</span>
+              <p>Following</p>
+            </div>
+          </div>
+          <a href={userGitHubURL}>Go to User's GitHub</a>
+        </div>
+        <h2>Listed Repositories</h2>
+        <section className='repos-grid'>
+          {userReposArray.map(repo => (
+            <div key={repo.id} className='repo-div'>
+              <div>
+                <a href={repo.html_url}>{repo.name}</a>
+                <p>{repo.description}</p>
+              </div>
+              <p>Updated at {new Date(repo.updated_at).toLocaleDateString('en-CA', dateOptions)}</p>
+            </div>
+          ))}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default User
